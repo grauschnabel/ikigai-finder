a191d58893b2176d1a2d3fef4b59a77c96ed6821
Martin Kaffanke <martin@kaffanke.info>
2025-03-23 18:00:14 +0100

    style: PHP CodeSniffer-Fehler in Tests behoben

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,8 @@
 <?php
 /**
  * PHPUnit bootstrap file.
+ *
+ * @package WP_Ikigai
  */
 
 $_tests_dir = getenv( 'WP_TESTS_DIR' );
@@ -27,10 +29,10 @@ require_once "{$_tests_dir}/includes/functions.php";
  * Manually load the plugin being tested.
  */
 function _manually_load_plugin() {
-	require dirname( dirname( __FILE__ ) ) . '/wp-ikigai.php';
+	require dirname( __DIR__ ) . '/wp-ikigai.php';
 }
 
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
 // Start up the WP testing environment.
-require "{$_tests_dir}/includes/bootstrap.php"; 
+require "{$_tests_dir}/includes/bootstrap.php";
